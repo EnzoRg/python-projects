a4bb663765cc8683a239c398e500ac0e3c16b634
Enzo Manolucos <57510827+EnzoRg@users.noreply.github.com>
2022-01-26 13:22:06 -0300

    Add geometry reshape

--- calculator/mini-calculator.py
+++ calculator/mini-calculator.py
@@ -1,16 +1,16 @@
 from tkinter import *
 
 
-def boton_dibujar(key, col, lin):
-    boton = Button(window, text=key, command=lambda: boton_press(key))
+def draw_button(key, col, lin):
+    boton = Button(window, text=key, command=lambda: press_button(key))
     boton.grid(column=col+1, row=lin+1)
     return boton
 
 
-def boton_press(key):
+def press_button(key):
     if key == "C":
         disp["text"] = ""
-    elif key == "<":
+    elif key == "←":
         disp["text"] = disp["text"][:-1]
     elif key == "=":
         disp["text"] = str(round(eval(disp["text"]), 6))
@@ -20,11 +20,13 @@ def boton_press(key):
 
 window = Tk()
 window.title("Calculadora")
+window.geometry("100x200")
+window.resizable(width=True, height=True)
 
 disp = Label(window, text="")
-disp.grid(column=0, row=0, columnspan=5)
+disp.grid(column=0, row=0, columnspan=5, sticky=W+E)
 
-keys = "()C<789/456*123-.0=+"
-buton_list = [boton_dibujar(keys[n], n % 4, n//4) for n in range(20)]
+keys = "()C←789/456*123-.0=+"
+buton_list = [draw_button(keys[n], n % 4, n//4) for n in range(20)]
 
 window.mainloop()
